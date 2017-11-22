de1b1a775155ecfc0cc28b64c7bcdf06bc2429e3
heze <heze@DESKTOP-Q0ETOEM>
2017-11-22 22:13:13 +0800

    eeem

--- src/Chapter08/HomeWork01.java
+++ src/Chapter08/HomeWork01.java
@@ -5,22 +5,21 @@ import java.io.*;
 public class HomeWork01 {
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		String password="";
-		boolean b=false;
-		for(int i=0;i<5;i++) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String password = "";
+		boolean b = false;
+		for (int i = 0; i < 5; i++) {
 			System.out.println("Please input password");
-			password=br.readLine();//'\n'或'\r'换行结束
-			if(password.equals("123456")) {
+			password = br.readLine();// '\n'或'\r'换行结束
+			if (password.equals("123456")) {
 				System.out.println("恭喜进入游戏");
-				b=true;	//标识符
+				b = true; // 标识符
 				break;
 			}
 		}
-		if(!b) {
+		if (!b) {
 			System.out.println("密码错误，结束游戏");
 			System.exit(0);
 		}
 	}
 }
-
