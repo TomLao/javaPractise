76f2bc96eba9f0bb524d75925d7683f95ba8a7b1
Tom Lao <1026022953@qq.com>
2018-01-03 23:11:40 +0800

    GUI

--- src/Experment01/Two.java
+++ src/Experment01/Two.java
@@ -10,12 +10,17 @@ public class Two {
 		space = " ";
 		name = JOptionPane.showInputDialog(null, "Enter your full name(first, middle, last)", "中间用空格隔开");
 
+		//思路一：subString提取各个字段
 		first = name.substring(0, name.indexOf(space)); // 提取name的从0到空格
-		name = name.substring(name.indexOf(space) + 1, name.length());// 将name缩短空格+1,到最后
+		name = name.substring(name.indexOf(space) + 1, name.length());// 截短，并放回所截部分。将name缩短空格+1,到最后
 		middle = name.substring(0, name.indexOf(space));
-		last = name.substring(name.indexOf(space) + 1, name.length());
-
+		last = name.substring(name.indexOf(space) + 1, name.length());		
 		monogram = first.substring(0, 1) + middle.substring(0, 1) + last.substring(0, 1);
+		
+//		//思路二：split按空格分切成一个数组
+//		String[] strArray=name.split(" ");	
+//		monogram = strArray[0].substring(0, 1) + strArray[1].substring(0, 1) + strArray[2].substring(0, 1);
+		
 		JOptionPane.showConfirmDialog(null, "your monogram is " + monogram);
 		// 查一下JOptionPane,showConfirmDialog
 	}
